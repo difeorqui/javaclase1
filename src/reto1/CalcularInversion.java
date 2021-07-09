9052977df44a12d5d1f6d1116544135f9d30479e
difeorqui <dfoq76@hotmail.com>
2021-07-08 20:47:50 -0500

    ejemplo reto 1

---
+++ src/reto1/CalcularInversion.java
@@ -0,0 +1,84 @@
+package reto1;
+
+/**
+ * CalcularInversion
+ */
+public class CalcularInversion {
+
+    // Atributos
+    private int tiempo;
+    private double capital;
+    private double interes;
+
+    // Métodos
+    // Constructor
+    public CalcularInversion() {
+        this.tiempo = 0;
+        this.capital = 0.0;
+        this.interes = 0.0;
+    }
+
+    // Constructor sobrecarga
+    public CalcularInversion(Integer tiempo, Double capital, Double interes) {
+        this.tiempo = tiempo;
+        this.capital = capital;
+        this.interes = interes;
+    }
+    
+    public double calcularInteresSimple() {
+        double interesSimple = this.capital * (this.interes / 100) * this.tiempo;
+        // divide entre 100 porque al ingresar el interés se ingresa como un valor
+        // entero, por ejemplo 19% se escribe 19 y la división lo convierte en 0.19
+        // Luego se multiplica por el capital para calcularle el interés
+        // Luego por el tiempo para calcularlo a lo largo del tiempo.
+        return Math.round(interesSimple);
+    }
+    
+    public double calcularInteresCompuesto() {
+        double interesCompuesto = this.capital * 
+                                  (Math.pow(1 + (this.interes * 100), this.tiempo) - 1);
+                                  // Math.pow, elevar un valor a otro, en este caso tiempo
+        return Math.round(interesCompuesto);
+    }
+
+    public double compararInversion() {
+        return calcularInteresCompuesto() - calcularInteresSimple();
+    }
+
+    // Otra forma de instaciar la clase
+    public double compararInvsersion(Integer tiempo, Double capital, Double interes) {
+        this.tiempo = tiempo;
+        this.capital = capital;
+        this.interes = interes;
+
+        return Math.round(compararInversion());
+    }
+
+    public double getCapital() {
+        return capital;
+    }
+
+    public double getInteres() {
+        return interes;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public static void main(String[] args) {
+        CalcularInversion inversion1 = new CalcularInversion(); // Llama al constructor
+        // Aquí todos los parámetros quedarían en cero.
+        System.out.println("Capital: " + inversion1.getCapital());
+        System.out.println("Interés: " + inversion1.getInteres());
+        System.out.println("Tiempo: " + inversion1.getTiempo());
+        
+        System.out.println(inversion1.calcularInteresSimple()); 
+        System.out.println(inversion1.calcularInteresCompuesto()); 
+        System.out.println(inversion1.compararInvsersion(12,2000000.0,5.0));
+
+    }
+
+
+} // Cierra la clase
+
