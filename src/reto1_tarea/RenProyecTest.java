3e1ce0875207f9aa46940247e2174bf1f496b95a
difeorqui <dfoq76@hotmail.com>
2021-07-13 18:26:45 -0500

    se incluye pdf con el enunciado del ejercicio

--- src/reto1_tarea/RenProyecTest.java
+++ src/reto1_tarea/RenProyecTest.java
@@ -1,15 +1,18 @@
 public class RenProyecTest {
     public static void main(String[] args) {
+        System.out.println("Caso de prueba 1");
         RenProyec Analisis1 = new RenProyec(2, 50000000.0, 6.2);
         System.out.println(Analisis1.calcularInteresSimple());
         System.out.println(Analisis1.calcularInteresCompuesto());
         System.out.println(Analisis1.analisisInversion());
-
+        
+        System.out.println("Caso de prueba 2");
         RenProyec Analisis2 = new RenProyec();
         System.out.println(Analisis2.calcularInteresSimple());
         System.out.println(Analisis2.calcularInteresCompuesto());
         System.out.println(Analisis2.analisisInversion(2, 50000000.0, 6.2));
 
+        System.out.println("Caso de prueba 3");
         RenProyec Analisis3 = new RenProyec(3, 15000000.0, 0.0);
         System.out.println(Analisis3.calcularInteresSimple());
         System.out.println(Analisis3.calcularInteresCompuesto());
