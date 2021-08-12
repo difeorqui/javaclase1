af9a8db5ab1dd347c2176626ea1597084dcabe94
difeorqui <dfoq76@hotmail.com>
2021-08-12 17:56:16 -0500

    Se agrega preliminar del reto 5

---
+++ src/reto5/reto4/src/main/java/utp/misiontic2022/c2/utp/misiontic2022/c2/p41/reto4/util/Requerimiento2_controller.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utp.misiontic2022.c2.utp.misiontic2022.c2.p41.reto4.util;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import utp.misiontic2022.c2.utp.misiontic2022.c2.p41.reto4.modelo.dao.Requerimiento2_Dao;
+import utp.misiontic2022.c2.utp.misiontic2022.c2.p41.reto4.modelo.vo.Requerimiento_2;
+
+/**
+ *
+ * @author Diego_Orozco
+ */
+public class Requerimiento2_controller {
+    private Requerimiento2_Dao ctrl;
+
+    public Requerimiento2_controller() {
+        this.ctrl = new Requerimiento2_Dao();
+    }
+    
+    public ArrayList<Requerimiento_2> consultar(){
+        return this.ctrl.consultar();
+    }
+    
+    public void close() throws SQLException{
+        this.ctrl.close();
+    }
+}
